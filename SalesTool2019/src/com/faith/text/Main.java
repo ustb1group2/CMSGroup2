885a254042e5173fe2af0534b19286bed35fecc6
MEGHALAL <MEGHALAL@LAPTOP-MUM7P075>
2019-09-23 17:15:14 +0530

    tst7

--- SalesTool2019/src/com/faith/text/Main.java
+++ SalesTool2019/src/com/faith/text/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	
 	private static void displayGreetings()
 	{
-		System.out.println("Welco to Sales guys!   29...");
+		System.out.println("Welco to Sales guys!!!!29...");
 		System.out.println("heyyyy guyss and !!!!");
 	}
 
