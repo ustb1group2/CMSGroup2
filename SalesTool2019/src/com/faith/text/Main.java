486621e627faf421f61c2c7216d9b994c39cb40f
MEGHALAL <MEGHALAL@LAPTOP-MUM7P075>
2019-09-23 15:57:13 +0530

    initial commit

---
+++ SalesTool2019/src/com/faith/text/Main.java
@@ -0,0 +1,20 @@
+package com.faith.text;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData objSalesData = new SalesData();
+		
+		System.out.println("hello UST Guys!");
+		displayGreetings();
+		objSalesData.display();
+
+	}
+	
+	private static void displayGreetings()
+	{
+		System.out.println("welcome to Sales Report  29...");
+	}
+
+}
---
+++ SalesTool2019/src/com/faith/text/SalesData.java
@@ -0,0 +1,10 @@
+package com.faith.text;
+
+public class SalesData {
+	
+	void display()
+	{
+		System.out.println("Sales Report!!!...");
+	}
+
+}
